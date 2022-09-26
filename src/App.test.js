81fefc51b122e0c9542a7890d81c1d01d718a6a9
Serch9 <46693564+Serch9@users.noreply.github.com>
2022-09-26 15:05:00 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
+  console.log("prueba");
   const linkElement = screen.getByText(/*learn react*/i);
   expect(linkElement).toBeInTheDocument();
 });
